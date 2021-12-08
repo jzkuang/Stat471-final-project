6edb3cfdc589b836339aba4a89a8c0e4fc264922
James Kuang <james.kuang.13@gmail.com>
2021-12-08 17:52:11 -0500

    cleaned data

--- code/Data Cleaning.R
+++ code/Data Cleaning.R
@@ -7,3 +7,5 @@ gradRate_filtered <- gradRate %>%
   filter(YEAR == "2019") %>% 
   filter(COHORT == "Combined") %>% 
   filter(!grepl("DISTRICT", ENTITY_NAME))
+
+#write.csv(gradRate_filtered, "Final Project/cleaned data/ACC HS Graduation Rate.xlsx")
