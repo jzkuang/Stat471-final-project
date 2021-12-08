8162cc07c0dc1e01ffae448a5078c3307ae66ee4
James Kuang <james.kuang.13@gmail.com>
2021-12-08 17:49:46 -0500

    More Data Cleaning

--- code/Data Cleaning.R
+++ code/Data Cleaning.R
@@ -1,2 +1,9 @@
 library(tidyverse)
 
+gradRate <- readxl::read_excel("Final Project/data/ACC HS Graduation Rate.xlsx")
+
+gradRate_filtered <- gradRate %>% 
+  filter(SUBGROUP_NAME == "All Students") %>% 
+  filter(YEAR == "2019") %>% 
+  filter(COHORT == "Combined") %>% 
+  filter(!grepl("DISTRICT", ENTITY_NAME))
