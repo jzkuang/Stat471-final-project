06e983003567afb084c4855acd1ee564d6b7ec58
Alex Chen <alche@sas.upenn.edu>
2021-12-11 14:12:34 -0500

    cleaned data

--- code/Data Cleaning Alex.R
+++ code/Data Cleaning Alex.R
@@ -7,45 +7,53 @@ gradRate_filtered <- gradRate %>%
   filter(YEAR == "2019") %>% 
   filter(COHORT == "Combined") %>% 
   filter(!grepl("DISTRICT", ENTITY_NAME))
-write.csv(gradRate_filtered, "Stat471-final-project/cleaned data/ACC HS Graduation Rate.csv")
+#write.csv(gradRate_filtered, "Stat471-final-project/cleaned data/ACC HS Graduation Rate.csv")
 
 #gave warnings, I'm guessing most of the warnings are because of columns that have no data
 #accountability, gives overall status of school
 accountability <- readxl::read_excel("Stat471-final-project/data/Accountability Status.xlsx")
 accountability_filtered <- accountability %>% 
   filter(YEAR == "2019") %>%
-  filter(is.na(OVERRIDE)) %>% #remove values where the status was overriden
-  select(-MADE_PROGRESS_FLAG, -CHANGE_STATUS_FLAG, -OVERRIDE)
-write.csv(accountability_filtered, "Stat471-final-project/cleaned data/Accountability.csv")
+  filter(is.na(OVERRIDE)) %>% #remove values where overall status was overriden
+  select(ENTITY_CD, OVERALL_STATUS, MADE_PROGRESS)
+#write.csv(accountability_filtered, "Stat471-final-project/cleaned data/Accountability.csv")
 
 
-#BOCES is a program of shared educational services provided to school districts 
-#Need index = Need-to-Resource Capacity Category
+#BOCES is a program of shared educational services provided to school districts. Should we keep this???
+#Need index = Need-to-Resource Capacity Category. The need/resource capacity index, a measure of a district's ability to meet the needs of its students with local
+# resources, is the ratio of the estimated poverty percentage1 (expressed in standard score form) to the Combined
+# Wealth Ratio2 (expressed in standard score form).
 BOCES <- readxl::read_excel("Stat471-final-project/data/BOCES and N_RC.xlsx")
 BOCES_filtered <- BOCES %>% 
-  filter(YEAR == "2019")
-write.csv(BOCES_filtered, "Stat471-final-project/cleaned data/BOCES.csv")
+  filter(YEAR == "2019") %>%
+  select(ENTITY_CD, BOCES_CD, NEEDS_INDEX)
+#write.csv(BOCES_filtered, "Stat471-final-project/cleaned data/BOCES.csv")
 
 
 #amount of money spent on the school
 expenditure <- readxl::read_excel("Stat471-final-project/data/Expenditures per Pupil.xlsx")
 expenditure_filtered <- expenditure %>% 
-  filter(YEAR == "2019")
-write.csv(expenditure_filtered, "Stat471-final-project/cleaned data/Expenditure.csv")
+  filter(YEAR == "2019") %>%
+  select(ENTITY_CD, PUPIL_COUNT_TOT, PER_FEDERAL_EXP, PER_STATE_LOCAL_EXP)
+#write.csv(expenditure_filtered, "Stat471-final-project/cleaned data/Expenditure.csv")
 
 #gave warnings
 #removed the 2020 only statistics
 inexperience <- readxl::read_excel("Stat471-final-project/data/Inexperienced Teachers and Principals.xlsx")
 inexperience_filtered <- inexperience %>% 
-  filter(YEAR == "2019") %>%
-  select(-TEACH_DATA_REP_FLAG, -PRIN_DATA_REP_FLAG, -TOT_PRINC_LOW, -TOT_PRINC_HIGH, -TOT_TEACH_LOW, -TOT_TEACH_HIGH)
-write.csv(inexperience_filtered, "Stat471-final-project/cleaned data/Inexperience.csv")
+  filter(YEAR == "2019") %>% 
+  select(ENTITY_CD, NUM_TEACH, PER_TEACH_INEXP, NUM_PRINC, PER_PRINC_INEXP) #UNSURE IF SHOULD USE TOTAL NUM OF TEACH AND PRINC
+#write.csv(inexperience_filtered, "Stat471-final-project/cleaned data/Inexperience.csv")
 
 #gave warnings
 #out of certification (teachers no certification)
 OOC <- readxl::read_excel("Stat471-final-project/data/Teachers Teaching Out of Certification.xlsx")
 OOC_filtered <- OOC %>% 
   filter(YEAR == "2019") %>%
-  select(-TOT_OUT_CERT_LOW, -TOT_OUT_CERT_HIGH, -OUT_OF_CERT_DATA_REP_FLAG)
-write.csv(OOC_filtered, "Stat471-final-project/cleaned data/Teachers Teaching Out of Certification.csv")
+  select(ENTITY_CD, PER_OUT_CERT)
+#write.csv(OOC_filtered, "Stat471-final-project/cleaned data/Teachers Teaching Out of Certification.csv")
 
+
+#Questions:
+#Should I keep total number of teachers? 
+#Do we have data on the pupil-teacher ratios for these schools? We could probably count that using total pupil and total teacher counts
