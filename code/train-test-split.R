a0a8babf367f29db5099974fed101ec17fde949f
Alex Chen <alche@sas.upenn.edu>
2021-12-12 18:28:15 -0500

    train test data

---
+++ code/train-test-split.R
@@ -0,0 +1,17 @@
+#train-test-split
+library(tidyverse)
+
+# read in the cleaned data
+nyschool_data = read_csv("Stat471-final-project/cleaned data/merged data.csv")
+
+# split into train and test
+set.seed(5) 
+n = nrow(nyschool_data)
+train_samples = sample(1:n, round(0.8*n))
+
+nyschool_train = nyschool_data[train_samples,]
+nyschool_test = nyschool_data[-train_samples,]
+
+# save the train and test data
+write_csv(nyschool_train, file = "Stat471-final-project/cleaned data/final data/nyschool_train.csv")
+write_csv(nyschool_test, file = "Stat471-final-project/cleaned data/final data/nyschool_test.csv")
