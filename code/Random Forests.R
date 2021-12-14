160305128df61115909e8e1c0f9b9d348d6691a0
Alex Chen <alche@sas.upenn.edu>
2021-12-14 12:17:44 -0500

    rf finished

--- code/Random Forests.R
+++ code/Random Forests.R
@@ -5,4 +5,47 @@ library(tidyverse)
 # read in the cleaned data
 nyschool_train = read_csv("Stat-471-final-project/cleaned data/final data/nyschool_train.csv") 
 
+set.seed(1)
 rf_fit = randomForest(GRAD_RATE ~ . -INSTITUTION_ID -ENTITY_NAME, data = nyschool_train)
+
+#plot OOB error as a function of number of trees
+plot(rf_fit)
+
+#tuning the random forest
+mvalues = seq(1,32, by = 2) #m range can be 1-32 
+oob_errors = numeric(length(mvalues))
+ntree = 200 #the OOB Error is stabilized at 200 trees
+for(idx in 1:length(mvalues)){
+  m = mvalues[idx]
+  rf_fit = randomForest(GRAD_RATE ~ . -INSTITUTION_ID -ENTITY_NAME, data = nyschool_train)
+  oob_errors[idx] = rf_fit$mse[ntree]
+}
+m_oob_errors = tibble(m = mvalues, oob_err = oob_errors)
+rf_tuning = m_oob_errors%>%
+  ggplot(aes(x = m, y = oob_err)) + 
+  geom_line() + geom_point() + 
+  scale_x_continuous(breaks = mvalues) +
+  labs(y = "OOB Error", x = "Value for m") +
+  theme_bw() 
+
+#saving the tuning for m
+pdf("Stat-471-final-project/results/rf_tuning.png")
+print(rf_tuning)
+dev.off()
+
+#m = 15 is the best mtry value, which is higher than default m = 10
+best_m = m_oob_errors %>%
+  arrange(oob_errors) %>% head(1) %>% pull(m)
+
+set.seed(1)
+rf_fit_tuned = randomForest(GRAD_RATE ~ . -INSTITUTION_ID -ENTITY_NAME, mtry = best_m, ntree = 500,
+                            importance = TRUE, data = nyschool_train)
+plot(rf_fit_tuned) #double check to see if OOB has flattened out (it does!)
+
+#save the randomforest model
+save(rf_fit_tuned,file = "Stat-471-final-project/results/rf_fit_tuned.RData")
+
+#HOW TO LOAD THE FOREST rf_fit_tuned = get(load("rf_fit_tuned.RData"))
+
+
+
