29837c857b86aadb83a2cb832c7aaf37212f7b9a
James Kuang <james.kuang.13@gmail.com>
2021-12-18 19:29:22 -0500

    rmd formatting

--- code/Exploratory Data Analysis.R
+++ code/Exploratory Data Analysis.R
@@ -8,11 +8,20 @@ mean(training$GRAD_RATE)
 # Avg 85.6%
 sd(training$GRAD_RATE)
 # SD 15.58
-ggplot(training) +
+
+dist_grad <- ggplot(training) +
   geom_histogram(aes(x = GRAD_RATE)) +
   theme_bw() +
   labs(x = "Graduation Rate", y = "Count")
+png(width = 6, 
+    height = 4,
+    res = 300,
+    units = "in", 
+    filename = "Stat-471-final-project/results/eda_grad_hist.png")
+print(dist_grad)
+dev.off()
 # The data has a left skew with most schools clustered around the mean
+
 training %>% 
   select_if(is.numeric) %>% 
   ggcorr(method = c("everything", "pearson"), layout.exp = 15, geom = "tile", max_size = 2)
