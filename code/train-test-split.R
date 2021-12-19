186977977dd757a585eba91ebf4a74c21ce74590
Alex Chen <alche@sas.upenn.edu>
2021-12-19 14:17:27 -0500

    done

--- code/train-test-split.R
+++ code/train-test-split.R
@@ -16,4 +16,3 @@ nyschool_test = nyschool_data[-train_samples,]
 write_csv(nyschool_train, file = "Stat-471-final-project/cleaned data/final data/nyschool_train.csv")
 write_csv(nyschool_test, file = "Stat-471-final-project/cleaned data/final data/nyschool_test.csv")
 
-write_csv(nyschool_test, file = "Stat-471-final-project/cleaned data/final data/nyschool_train.csv")
