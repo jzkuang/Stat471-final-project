d65841763a4bd9978b11c462e503dcec34ea0f59
James Kuang <james.kuang.13@gmail.com>
2021-12-19 21:14:43 -0500

    fixing run-all

--- code/1-Data-Cleaning.R
+++ code/1-Data-Cleaning.R
@@ -67,14 +67,14 @@ write.csv(accountability_filtered, "Stat-471-final-project/cleaned data/Accounta
 #Need index = Need-to-Resource Capacity Category. The need/resource capacity index, a measure of a district's ability to meet the needs of its students with local
 # resources, is the ratio of the estimated poverty percentage1 (expressed in standard score form) to the Combined
 # Wealth Ratio2 (expressed in standard score form).
-BOCES <- readxl::read_excel("Stat-471-final-project/data/BOCES and N_RC.xlsx")
+BOCES <- readxl::read_excel("stat-471-final-project/data/BOCES and N_RC.xlsx")
 BOCES_filtered <- BOCES %>% 
   filter(YEAR == "2019") %>%
   select(ENTITY_CD, BOCES_CD, NEEDS_INDEX)
 write.csv(BOCES_filtered, "Stat-471-final-project/cleaned data/BOCES.csv", row.names = FALSE)
 
 #amount of money spent on the school
-expenditure <- readxl::read_excel("Stat471-final-project/data/Expenditures per Pupil.xlsx")
+expenditure <- readxl::read_excel("stat-471-final-project/data/Expenditures per Pupil.xlsx")
 expenditure_filtered <- expenditure %>% 
   filter(YEAR == "2019") %>%
   select(ENTITY_CD, PUPIL_COUNT_TOT, PER_FEDERAL_EXP, PER_STATE_LOCAL_EXP)
@@ -82,17 +82,17 @@ write.csv(expenditure_filtered, "Stat-471-final-project/cleaned data/Expenditure
 
 #gave warnings
 #removed the 2020 only statistics
-inexperience <- readxl::read_excel("Stat471-final-project/data/Inexperienced Teachers and Principals.xlsx")
+inexperience <- readxl::read_excel("stat-471-final-project/data/Inexperienced Teachers and Principals.xlsx")
 inexperience_filtered <- inexperience %>% 
   filter(YEAR == "2019") %>% 
   select(ENTITY_CD, NUM_TEACH, PER_TEACH_INEXP, NUM_PRINC, PER_PRINC_INEXP) #UNSURE IF SHOULD USE TOTAL NUM OF TEACH AND PRINC
-write.csv(inexperience_filtered, "Stat-471-final-project/cleaned data/Inexperience.csv", row.names = FALSE)
+write.csv(inexperience_filtered, "stat-471-final-project/cleaned data/Inexperience.csv", row.names = FALSE)
 
 #gave warnings
 #out of certification (teachers no certification)
-OOC <- readxl::read_excel("Stat-471-final-project/data/Teachers Teaching Out of Certification.xlsx")
+OOC <- readxl::read_excel("stat-471-final-project/data/Teachers Teaching Out of Certification.xlsx")
 OOC_filtered <- OOC %>% 
   filter(YEAR == "2019") %>%
   select(ENTITY_CD, PER_OUT_CERT)
-write.csv(OOC_filtered, "Stat-471-final-project/cleaned data/Teachers Teaching Out of Certification.csv", row.names = FALSE)
+write.csv(OOC_filtered, "stat-471-final-project/cleaned data/Teachers Teaching Out of Certification.csv", row.names = FALSE)
 
